d98be4a9464831ed33019a816a1c8abce8697734
Kelly <84016657+telkelly@users.noreply.github.com>
2023-10-25 18:47:27 +0300

    started app accounts with login.html creates login view

---
+++ event_platform/accounts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
+# Create your views here.
+
+def user_login(request):
+    return render(request, 'accounts/login.html')
