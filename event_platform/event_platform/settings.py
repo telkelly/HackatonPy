aea62d27b66c34a3da7aec1339322cb2009d35fc
Kelly <84016657+telkelly@users.noreply.github.com>
2023-11-28 14:55:37 +0200

    changed settings

--- event_platform/event_platform/settings.py
+++ event_platform/event_platform/settings.py
@@ -134,11 +134,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # STATIC_URL = 'static/'
-STATIC_URL = 'https://meet-there-site.onrender.com/static/'
+STATIC_URL = '/static/'
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
