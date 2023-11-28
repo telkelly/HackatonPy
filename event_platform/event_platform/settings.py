8150d63fbc84257e6edbccce7e2acb932e655b97
Kelly <84016657+telkelly@users.noreply.github.com>
2023-11-28 14:41:57 +0200

    changed settings

--- event_platform/event_platform/settings.py
+++ event_platform/event_platform/settings.py
@@ -133,12 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_URL = 'https://meet-there-site.onrender.com/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'https://meet-there-site.onrender.com/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
