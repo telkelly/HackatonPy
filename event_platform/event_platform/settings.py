ec7e55901f27d4e8a155f61e556be5d4276246c8
Kelly <84016657+telkelly@users.noreply.github.com>
2024-03-27 19:32:15 +0200

    changed settings

--- event_platform/event_platform/settings.py
+++ event_platform/event_platform/settings.py
@@ -102,7 +102,7 @@ USE_TZ = True
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
